790ed9af2ce969bcfe655b981281a4231aff8829
Duane17 <duanegadama@gmail.com>
2024-05-23 09:39:44 +0200

    integration with mongodb, deployment to net & validation

---
+++ models/person.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log(`connecting to ${url}`)
+
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch(error => {
+        console.log(`error connecting to MongoDB: ${error.message}`)
+    })
+
+const validatePhoneNumber = (number) => {
+    const regex = /^\d{2,3}-\d+$/;
+    return regex.test(number) && number.length >= 8;
+};
+
+const personSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minLength: 3,
+        required: true,
+    },
+    number: {
+        type: String,
+        validate: {
+            validator: validatePhoneNumber,
+            message: props => `${props.value} is not a valid number`
+        }
+    },
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
+  
---
+++ mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node <script-file> <password> [<name> <number>]');
+    process.exit(1);
+}
+
+const password = process.argv[2]
+const argName = process.argv[3]
+const argNumber = process.argv[4]
+
+const url = `mongodb+srv://duanegadama:${password}@cluster0.4zhouvb.mongodb.net/phoneApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+    // fecth and display all entries in phone book
+    Person.find({}).then(result => {
+        console.log('phonebook:');
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`);
+        })
+        mongoose.connection.close()
+    })
+} else if (process.argv.length === 5) {
+    // Add new entry to phone book
+    const person = new Person({
+        name: argName,
+        number: argNumber,
+    })
+
+    person.save().then(result => {
+        console.log(`added ${argName} number ${argNumber} to phonebook`);
+        mongoose.connection.close()
+    })
+
+} else {
+    console.log('Please provide both name and number to add a new entry, or just the password to list all entries');
+    process.exit(1);
+}
